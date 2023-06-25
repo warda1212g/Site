950109c09e7275c37f3730f9c4deac97b4449d4f
warda1212g <132137490+warda1212g@users.noreply.github.com>
2023-06-25 00:14:00 -0700

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,31 @@
+  
+// function validate(){
+  
+//   let password=document.querySelector("#password").value;
+//   let email=document.querySelector("#email").value;
+
+//  if(email == "hello@gmail.com" && password == "123"){
+//   window.location.assign("user.html");
+//   alert("login successfully")
+//   return true ;
+     
+       
+//     } 
+//  else{
+//   alert("Wrong password")
+//   return false;   
+//  }
+// }
+
+    
+    
+
+    let myimage = ["ii1.jpg","ii2.jpg","gg.png"]
+    function arraycall(){
+      document.getElementById("images1").src = myimage[0];
+     
+    }
+    function saveProfile(){
+      alert("profile saved!");
+    } 
+
